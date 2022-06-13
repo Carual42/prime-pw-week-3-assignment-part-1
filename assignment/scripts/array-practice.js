3a103bb287720196a906de3e6a1fb79abc576c49
Alex Carufel <Carual42@gmail.com>
2022-06-12 19:41:43 -0500

    finished

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -87,16 +87,27 @@ console.log('The meals are now', foodArray);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
-
+foodArray[1] = 'Hash browns'
+console.log(foodArray)
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
-
+foodArray.sort();
+console.log(foodArray);
+foodArray.reverse();
+console.log(foodArray);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-
+let text = "";
+foodArray.forEach(andText);
+ 
+function andText(foodArray) {
+  text += foodArray + " and " }
+  console.log(text)
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let isItFoodOrPet = foodArray.concat(animalArray)
+console.log(isItFoodOrPet)
